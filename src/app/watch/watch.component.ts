7d9ca8cb9a2b137404cdb710ea2ebbc1025f79d0
Nadjide.O <106732960+Nadjide@users.noreply.github.com>
2024-01-06 21:49:49 +0100

    Delete filter component files and add sorting functionality to watch component

--- src/app/watch/watch.component.ts
+++ src/app/watch/watch.component.ts
@@ -19,7 +19,7 @@ export class WatchComponent implements OnInit {
       price: 10000,
       image:
         'https://lemag.cresus.fr/wp-content/uploads/2023/06/56570-rolex-milgauss-3632opt-e1687955024296.jpg',
-      date: new Date(),
+      date: new Date(2023, 6, 1),
       isFavorite: false,
     },
     {
@@ -27,7 +27,7 @@ export class WatchComponent implements OnInit {
       price: 5000,
       image:
         'https://i.ebayimg.com/00/s/MTYwMFgxNDg2/z/MAQAAOSw7YxkCLUt/$_1.JPG',
-      date: new Date(),
+      date: new Date(2022, 3, 15),
       isFavorite: false,
     },
     {
@@ -35,14 +35,14 @@ export class WatchComponent implements OnInit {
       price: 15000,
       image:
         'https://www.frojo.com/218596-home_default/montre-big-bang-chronograph-44-mm-acier-cadran-noir-bracelet-caoutchouc-noir.jpg',
-      date: new Date(),
+      date: new Date(2023, 1, 10),
       isFavorite: false,
     },
     {
       name: 'Cartier',
       price: 8000,
       image: 'https://i.ebayimg.com/images/g/tWAAAOSwOEBlH0Lo/s-l1200.webp',
-      date: new Date(),
+      date: new Date(2022, 11, 5),
       isFavorite: false,
     },
     {
@@ -50,7 +50,7 @@ export class WatchComponent implements OnInit {
       price: 6000,
       image:
         'https://www.tagheuer.com/on/demandware.static/-/Library-Sites-TagHeuer-Shared/default/dwb9b4d31b/images/sprites/Formula1/WAZ1118.BA0875/RTW_backUp.jpg',
-      date: new Date(),
+      date: new Date(2022, 8, 20),
       isFavorite: false,
     },
     {
@@ -58,44 +58,66 @@ export class WatchComponent implements OnInit {
       price: 9000,
       image:
         'https://www.breitling.com/media/image/2/gallery_square_700/asset-version-87be0b6013/ab0138211b1a1-navitimer-b01-chronograph-43-three-quarter.png',
-      date: new Date(),
+      date: new Date(2023, 4, 30),
       isFavorite: false,
     },
     {
       name: 'Patek Philippe',
       price: 20000,
-      image:
-        'https://pics.zeitauktion.com/2023/2300998_sw6_theme2_2k.jpg',
-      date: new Date(),
+      image: 'https://pics.zeitauktion.com/2023/2300998_sw6_theme2_2k.jpg',
+      date: new Date(2023, 2, 12),
       isFavorite: false,
     },
-  ]
+  ];
   filteredWatches = this.watches;
-  ;
-
   constructor() {}
 
   ngOnInit(): void {
     const storedFavorites = localStorage.getItem('favorites');
     if (storedFavorites) {
       const favorites = JSON.parse(storedFavorites);
-      this.watches = this.watches.map(watch => ({
+      this.watches = this.watches.map((watch) => ({
         ...watch,
         isFavorite: favorites.includes(watch.name),
       }));
     }
 
     this.filteredWatches = this.watches;
-
   }
 
   toggleFavorite(watch: Watch): void {
     watch.isFavorite = !watch.isFavorite;
-    const favorites = this.watches.filter(w => w.isFavorite).map(w => w.name);
+    const favorites = this.watches
+      .filter((w) => w.isFavorite)
+      .map((w) => w.name);
     localStorage.setItem('favorites', JSON.stringify(favorites));
   }
 
   handleSearchEvent(searchTerm: string): void {
-    this.filteredWatches = this.watches.filter(watch => watch.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    this.filteredWatches = this.watches.filter((watch) =>
+      watch.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+
+  sortNameAsc = true;
+  sortDateAsc = true;
+
+  sortByDate() {
+    this.filteredWatches.sort((a, b) => {
+      let dateA = new Date(a.date);
+      let dateB = new Date(b.date);
+      return this.sortDateAsc ? dateA.getTime() - dateB.getTime() : dateB.getTime() - dateA.getTime();
+    });
+    this.sortDateAsc = !this.sortDateAsc;
+  }
+
+  // Tri par nom
+  sortByName() {
+    this.filteredWatches.sort((a, b) => {
+      let nameA = a.name.toUpperCase();
+      let nameB = b.name.toUpperCase();
+      return this.sortNameAsc ? (nameA < nameB ? -1 : (nameA > nameB ? 1 : 0)) : (nameA > nameB ? -1 : (nameA < nameB ? 1 : 0));
+    });
+    this.sortNameAsc = !this.sortNameAsc;
   }
 }
