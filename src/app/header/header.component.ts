25e1464320cddc6948a9ebaf8c9a933bd4d3a2c0
Nadjide.O <106732960+Nadjide@users.noreply.github.com>
2024-01-16 14:30:11 +0100

    ajout d'une pastille rouge dans le header avec le nombre d'elements dans le panier

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    RouterModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    CommonModule,
+  ],
   template: `
     <header class="header">
       <nav class="navbar">
@@ -16,11 +23,30 @@ import { RouterLinkActive } from '@angular/router';
           <li><a routerLink="/contact">Contact</a></li>
         </ul>
         <div class="cart">
-          <a class="panier-link" href="/panier">Panier</a>
+          <div class="panier-container">
+            <a class="panier-link" href="/panier">Panier</a>
+            <span class="cart-item-count" *ngIf="cartItemCount > 0">{{
+              cartItemCount
+            }}</span>
+          </div>
         </div>
       </nav>
     </header>
   `,
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  cartItemCount = 0;
+
+  ngOnInit(): void {
+    this.updateCartItemCount();
+  }
+
+  updateCartItemCount(): void {
+    let cart = JSON.parse(localStorage.getItem('panier')!) || [];
+    this.cartItemCount = cart.reduce(
+      (count: any, item: { quantity: any }) => count + item.quantity,
+      0
+    );
+  }
+}
