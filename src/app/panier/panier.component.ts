0d10da87f907986fb2a3f71759553b42a6e50fa7
Nadjide.O <106732960+Nadjide@users.noreply.github.com>
2024-01-16 11:56:32 +0100

    footer corriger et ajout des total dans le panier

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { WatchService } from '../watch.service';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-panier',
   standalone: true,
-  imports: [MatCardModule, CommonModule],
+  imports: [MatCardModule, CommonModule,MatButtonModule],
   templateUrl: './panier.component.html',
   styleUrl: './panier.component.css',
 })
@@ -18,5 +20,14 @@ export class PanierComponent implements OnInit {
   ngOnInit(): void {
     this.watches = JSON.parse(localStorage.getItem('panier')!);
     console.log(this.watches);
+  }
+
+  removeWatch(watch: any): void {
+    this.watches = this.watches.filter((w) => w.id !== watch.id);
+    localStorage.setItem('panier', JSON.stringify(this.watches));
+  }
+
+  getTotal(): number {
+    return this.watches.reduce((acc, watch) => acc + watch.price, 0);
   }
 }
